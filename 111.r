8b78de2885514009db2e34eaf49a3e7f94c6c4be
Oliverckb <64025096+Oliverckb@users.noreply.github.com>
2020-09-04 12:12:48 +0200

    Add files via upload

---
+++ 111.r
@@ -0,0 +1,5 @@
+library(markmyassignment)
+lab_path <- "https://raw.githubusercontent.com/STIMALiU/AdvRCourse/master/Labs/Tests/lab2.yml"
+set_assignment(lab_path)
+source("lab2.r")
+mark_my_assignment()
